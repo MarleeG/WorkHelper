5dd23a64a9c9d380baaffecbf27ded0c3cdb8aac
Christina Gerard <ChristinaGerard@Christinas-MBP.hsd1.ga.comcast.net>
2020-03-27 21:02:53 -0400

    method selected and logged in UI console

--- src/shared/components/UIElement/MethodButton.js
+++ src/shared/components/UIElement/MethodButton.js
@@ -3,24 +3,33 @@ import { ButtonGroup, Button } from 'react-bootstrap';
 
 
 const MethodButton = props => {
-    const methodText = ["Remove Space(s)", "Remove New Line Character(s)", "Remove Slashes"];
-    console.log(props);
+    // const methodOptions = ["Remove Space(s)", "Remove New Line Character(s)", "Remove Slashes"];
+    const methodOptions = [
+        { displayText: "Remove Space(s)", name: "remove_space" },
+        { displayText: "Remove New Line Character(s)", name: "remove_new_line_char" },
+        { displayText: "Remove Slashes", name: "remove_slashes" }];
 
     return (
         <Fragment>
-            {/* <h3 className="text-center">Method Button</h3> */}
             <div className="mt-4">
-            <ButtonGroup aria-label="Method Options" size="lg">
-                {methodText.map((mthd, idx) => {
-                    return (
-                        <Button variant="dark" key={idx} onClick={() => props.updateInput(true)}>
-                            {mthd}
-                        </Button>
-                    )
-                })}
-            </ButtonGroup>
+                <strong>
+                    <h4 className="my-2">Select a method</h4>
+                </strong>
+                
+                <ButtonGroup aria-label="Method Options" size="lg">
+                    {methodOptions.map((mthd, idx) => {
+                        return (
+                            <Button
+                                variant="dark"
+                                key={idx}
+                                onClick={() => { props.updateInput(true); props.methodChange(mthd.name) }}
+                            >
+                                {mthd.displayText}
+                            </Button>
+                        )
+                    })}
+                </ButtonGroup>
             </div>
-            
         </Fragment>
     )
 }
